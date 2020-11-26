55a16ee101d2e74dde77285f9f74e2f6ee638c39
Nadun <nickcobolt123@gmail.com>
2020-11-27 03:01:23 +0530

    Basic Gameplay start

---
+++ Infinity Arrow/Assets/Scirpts/Cam controller/CamController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamController : MonoBehaviour
+{
+    public GameObject Player;
+
+    Vector3 offSet;
+    void Start()
+    {
+        offSet = this.transform.position - Player.transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = Player.transform.position + offSet; 
+    }
+}
---
+++ Infinity Arrow/Assets/Scirpts/Platform Controller/PlatformManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+
+
+public class PlatformManager : MonoBehaviour
+{
+    public List<GameObject> Platforms;
+
+
+    public static PlatformManager instanse;
+    private void Awake()
+    {
+        if (instanse)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            instanse = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+    }
+}
