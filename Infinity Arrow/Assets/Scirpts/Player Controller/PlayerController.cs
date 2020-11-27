bac4ee70e07fb3f2a0cabdf014184a394bd6a9fc
Nadun <nickcobolt123@gmail.com>
2020-11-28 02:15:16 +0530

    Debug

--- Infinity Arrow/Assets/Scirpts/Player Controller/PlayerController.cs
+++ Infinity Arrow/Assets/Scirpts/Player Controller/PlayerController.cs
@@ -7,19 +7,23 @@ public class PlayerController : MonoBehaviour
     private Vector3 touchPos;
     private Rigidbody2D rb;
     private Vector3 direction;
+
+    public float velocityMag;
+
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
-   // void FixedUpdate()
-   // {
-   //     
-   //     //.Translate(Vector3.up * Time.deltaTime * 5f);
-   // }
+    // void FixedUpdate()
+    // {
+    //     
+    //     //.Translate(Vector3.up * Time.deltaTime * 5f);
+    // }
 
-    private void FixedUpdate()
+    private void Update()
     {
+        transform.Translate(Vector2.up * Time.deltaTime * 8f);
         if (Input.touchCount > 0)
         {
             Touch touch = Input.GetTouch(0);
@@ -27,29 +31,25 @@ public class PlayerController : MonoBehaviour
             touchPos.z = 0f;
             direction = (touchPos - transform.position);
 
-            //rb.velocity = new Vector2(direction.x * Time.deltaTime * 200f, rb.velocity.y);
+            rb.velocity = new Vector2(direction.x * Time.deltaTime * 200f, 0f);
 
             if (touch.phase == TouchPhase.Ended)
             {
-                rb.velocity = new Vector2(0, 1f * Time.deltaTime * 200f);
+                rb.velocity = Vector2.zero;
             }
-            else
-            {
-                rb.velocity = new Vector2(direction.x * Time.deltaTime * 200f, 1f * Time.deltaTime * 200f);
-            }
+
             // //transform.position = new Vector3(touchPos.x, transform.position.y, transform.position.z);
         }
-        else
-        {
-            rb.velocity = new Vector2(0f, 1f * Time.deltaTime * 200f);
-        }
+
+
     }
+
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Platforms"))
         {
-            Instantiate(PlatformManager.instanse.Platforms[Random.Range(0, 2)],new Vector2(0, transform.position.y + 40f), Quaternion.identity);
+            Instantiate(PlatformManager.instanse.Platforms[Random.Range(0, 2)], new Vector2(0, transform.position.y + 40f), Quaternion.identity);
         }
 
         if (collision.gameObject.CompareTag("Enemy"))
